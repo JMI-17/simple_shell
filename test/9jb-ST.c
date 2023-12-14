71e39c435b44359267ac16c292106542444f2b6f
Lady-Bee <bisimi16@gmail.com>
2023-12-13 16:33:59 -0800

    task 5

--- test/9jb-ST.c
+++ test/9jb-ST.c
@@ -3,42 +3,46 @@
 #include <string.h>
 #include <errno.h>
 
-int custom_setenv(const char *name, const char *value, int overwrite) {
-    char *env_entry;
-    size_t len = strlen(name) + strlen(value) + 2;
-    env_entry = (char *)malloc(len);
+/**
+ * custom_setenv - prog set and unsets environment variable
+ * @name: name of variable
+ * @value: variable value
+ * @overwrite:variable
+ * Return: Always 0
+ */
 
-    if (name == NULL || name[0] == '\0' || strchr(name, '=') != NULL) {
-        char error_msg[] = "Invalid variable name\n";
-        write(STDERR_FILENO, error_msg, sizeof(error_msg) - 1);
-        return -1;
-    }
+int custom_setenv(const char *name, const char *value, int overwrite)
+{
+	size_t len = strlen(name) + strlen(value) + 2;
+	char *env_entry = (char *)malloc(len);
 
-    if (!overwrite && getenv(name) != NULL) {
-        char error_msg[] = "Variable already exists\n";
-        write(STDERR_FILENO, error_msg, sizeof(error_msg) - 1);
-        return 0;
-    }
+	if (name == NULL || name[0] == '\0' || strchr(name, '=') != NULL)
+	{
+		write(STDERR_FILENO, "Invalid variable name\n", 21);
+		return (-1);
+	}
+	if (!overwrite && getenv(name) != NULL)
+	{
+		write(STDERR_FILENO, "Variable already exists\n", 24);
+		return (0);
+	}
 
-    if (env_entry == NULL) {
-        char error_msg[] = "Memory allocation error\n";
-        write(STDERR_FILENO, error_msg, sizeof(error_msg) - 1);
-        return -1;
-    }
+	if (env_entry == NULL)
+	{
+		write(STDERR_FILENO, "Memory allocation error\n", 23);
+		return (-1);
+	}
+	strcpy(env_entry, name);
+	strcat(env_entry, "=");
+	strcat(env_entry, value);
 
-    strcpy(env_entry, name);
-    strcat(env_entry, "=");
-    strcat(env_entry, value);
-
-    if (putenv(env_entry) != 0) {
-        char error_msg[] = "Failed to set environment variable: ";
-        write(STDERR_FILENO, error_msg, sizeof(error_msg) - 1);
-        write(STDERR_FILENO, strerror(errno), strlen(strerror(errno)));
-        write(STDERR_FILENO, "\n", 1);
-        free(env_entry);
-        return -1;
-    }
-
-    return 0;
+	if (putenv(env_entry) != 0)
+	{
+		write(STDERR_FILENO, "Failed to set environment variable: ", 36);
+		write(STDERR_FILENO, strerror(errno), strlen(strerror(errno)));
+		write(STDERR_FILENO, "\n", 1);
+		free(env_entry);
+		return (-1);
+	}
+	return (0);
 }
-
--- test/main.h
+++ test/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+extern char **environ;
 int custom_setenv(const char *name, const char *value, int overwrite);
 
 #endif /* MAIN_H */
